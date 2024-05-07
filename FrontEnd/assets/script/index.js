37a7fa79acbad16cdbfb7deaad0c1331059afb2e
Céline <lavalleceline@yahoo.com>
2024-05-07 14:32:52 +0200

    Ajout d'une fonction pour afficher les images

---
+++ FrontEnd/assets/script/index.js
@@ -0,0 +1,21 @@
+(async() => {
+    console.log('before start');
+  
+    const reponse = await fetch('http://localhost:5678/api/works');
+    const works = await reponse.json();
+    
+    console.log(works);
+  })();
+
+  const works = document.querySelector('gallery');
+
+  works.forEach(function(work) {
+    const figure = document.createElement('figure');
+    let img = document.createElement('img');
+    let figcaption = document.createElement('figcaption');
+    img.src = work.imageUrl;
+    figure.classList.add("gallery");
+    works.appendChild(figure);
+    works.appendChild(img);
+    works.appendChild(figcaption);
+  })
