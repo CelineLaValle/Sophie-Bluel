b6020ba771d90fb7fb551b0842df660023c186a9
Céline <lavalleceline@yahoo.com>
2024-05-24 15:19:22 +0200

    Modification du login

--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -62,30 +62,47 @@ const form = document.querySelector('#loginForm');
 const errorMessage = document.querySelector('.error');
 
 
-  async function postUsers() {
+  async function postUsers(email, password) {
     try {
       const response = await fetch('http://localhost:5678/api/users/login', {
+        // Envoi de la requête à l'API
         method: 'POST',
+        // Spécifie que le contenu de la requête est au format JSON
         headers: {
           'Content-Type': 'application/json'
         },
+         // Corps de la requête avec email et mot de passe au format JSON
         body: JSON.stringify({
           email: email,
           password: password
         })
       })
+      // Attente de la réponse de l'API
       const user = await response.json();
-    } catch (error) {
+      // Si l'utilisateur a un token, il est connecté
+      if (user.token) {
+        // Stocke le token dans la session
+        window.sessionStorage.setItem('token', user.token);
+        // Redirection vers la page index.html
+        window.location.href = '../index.html';
+        //Sinon, affiche un message d'erreur
+      } else {
       showError();
     }
+    // En cas d'échec de réponse de l'API
+    } catch (error) {
+      console.error('Erreur lors de la connexion :', error);
+    }
+    
   }
+  
 
-// window.location.href = '../index.html';
 
   function showError() {
     email.classList.add('input-error');
     password.classList.add('input-error');
     errorMessage.textContent = "Votre email ou votre mot de passe est incorrect";
+
   }
 
   // On écoute l'envoi (submit)
