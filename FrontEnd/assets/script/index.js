f936957c199d393a0e06a661d23889bae4a8406f
Céline <lavalleceline@yahoo.com>
2024-05-27 17:29:35 +0200

    Deleting some comments

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -1,28 +1,3 @@
-// (async() => {
-//     console.log('before start');
-  
-//     const reponse = await fetch('http://localhost:5678/api/works');
-//     const works = await reponse.json();
-    
-//     console.log(works);
-//   })();
-
-//   const works = document.querySelector('.gallery');
-
-//   async function displayWorks() {
-//     const arrayWorks - await getWorks();
-//     works.forEach(function(work) {
-//     const figure = document.createElement('figure');
-//     let img = document.createElement('img');
-//     let figcaption = document.createElement('figcaption');
-//     img.src = work.imageUrl;
-//     figcaption.textContent = work.title;
-//     figure.classList.add("gallery");
-//     works.appendChild(figure);
-//     works.appendChild(img);
-//     works.appendChild(figcaption);
-//   })}
-
 /* fonction qui retourne le tableau */
 async function getWorks () {
   const reponse = await fetch('http://localhost:5678/api/works');
--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -1,66 +1,7 @@
-
-// Récupération des users
-
-// async function postUsers(userData) {
-//     const response = await fetch('http://localhost:5678/api/users/login', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-
-//       body: JSON.stringify({
-//         "email": "sophie.bluel@test.tld",
-//         "password": "S0phie"
-//       })
-//     });
-//     return await response.json();
-//   }
-
-//   postUsers();
-
-
-
-// // Fonction de connexion
-
-
-
-
-
-
-
-// async function loginUser() {
-//     // Récupération des users dans la fonction
-//     const users = await postUsers();
-//     //On écoute l'envoi (submit)
-//     form.addEventListener("submit", (e) => {
-//         // Pour éviter le rechargement de la page apres le submit
-//         e.preventDefault();
-//         // Récupération de la valeur des inputs email et password
-//         const userEmail = email.value;
-//         const userPassword = password.value;
-
-//         users.forEach(user => {
-//             // Vérification de l'email et du mot de passe
-//             if (userEmail === user.email && userPassword === user.password) {
-//                 // Si l'email et le mot de passe sont correct, cela valide la connexion
-//                 window.sessionStorage.loged = true;
-//         } else {
-//             // Sinon, on met les input en rouge et on affiche un message d'erreur
-//             email.classList.add('input-error')
-//             password.classList.add('input-error')
-//             errorMessage.textContent = "Votre email ou votre mot de passe est incorrect";
-//         }
-//         })
-//     })
-// }
-
-// loginUser();
-
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const form = document.querySelector('#loginForm');
 const errorMessage = document.querySelector('.error');
-
 
   async function postUsers(email, password) {
     try {
@@ -121,35 +62,6 @@ const errorMessage = document.querySelector('.error');
 }
 
 // Quand l'utilisateur est connecté
-
-// // Récupération du token stocké dans sessionStorage dans la variable token
-// const token = window.sessionStorage.getItem('token');
-
-// const vérifyTokenIsPresent = () =>  {
-//   const login_button = document.getElementById("id_login_button");
-//   // Récupération de la valeur "href" de la variable login-button et la stocke dans cette variable
-//   const hrefOrigin = login_button.getAttribute("href");
-//   // Si le token existe alors il est connecté
-//   if(token) {
-//     // Supprime le lien de connexion
-//     login_button.removeAttribute("href");
-//     // Remplace le texte par logout
-//     login_button.textContent = "logout";
-//     // Ajoute un gestionnaire d'événement pour le clic sur le bouton logout
-//     login_button.addEventListener("click", e => {
-//       e.preventDefault();
-//       // Supprime le token de la sessionStorage
-//       window.sessionStorage.removeItem("token");
-//       // Remet le lien de connexion
-//       login_button.setAttribute("href", hrefOrigin);
-//       // Change le texte par login
-//       login_button.textContent = "login";
-//     });
-//   }
-// }
-
-
-// vérifyTokenIsPresent();
 
 // Récupération du token stocké dans sessionStorage dans la variable token
 const token = window.sessionStorage.getItem('token');
