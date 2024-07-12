3948cd0eb1caf47f3685368f829a4625d5b2d3d9
Céline <lavalleceline@yahoo.com>
2024-07-12 16:24:12 +0200

    Nettoyage et tri du code

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -35,6 +35,13 @@ async function displayWorks() {
 
 displayWorks();
 
+
+
+
+
+
+// FILTRES
+
 const filtersContainer = document.querySelector('.filters');
 
 // Fonction qui affiche les filtres
@@ -57,15 +64,6 @@ async function displayButton() {
   addEventOnButtons();
   addFilterEvents();
 }
-
-// const filters = document.querySelectorAll('.filter');
-// console.log(filters);
-
-// if (tokenContent) {
-//   filters.forEach(element => {
-//     element.style.display = 'none';
-//   });
-// }
 
 // Fonction qui met un évènement sur mes boutons
 
@@ -106,15 +104,7 @@ function filtersWorks(categoryBtn) {
   })
 }
 
-
-// Sélectionner le premier filtre "Tous" si aucun autre filtre n'est sélectionné
-// const firstFilter = document.querySelector('.filter');
-// if (firstFilter) {
-//   firstFilter.classList.add('clicked');
-//   }
-
 // Couleur des filtres lorsqu'ils sont sélectionnés
-// Fonction pour définir les événements sur les filtres
 function addFilterEvents() {
   const filters = document.querySelectorAll('.filter');
 
@@ -130,16 +120,11 @@ function addFilterEvents() {
 }
 
 
-// Bande mode édition
 
-const header = document.querySelector(".header");
-const adminDiv = document.querySelector(".banner-admin");
-const adminP = document.createElement("p");
-adminDiv.classList.add("banner-admin");
-adminP.classList.add("banner-p");
-adminP.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> Mode édition';
-adminDiv.appendChild(adminP);
-header.appendChild(adminDiv);
+
+
+
+// MODALE
 
 // Fonction modal
 
@@ -159,6 +144,26 @@ closeButtons.forEach((closeButton) => {
     dialog.close();
   });
 })
+
+
+// Fonction afficher l'une ou l'autre modal
+
+function showModal() {
+  const addPhoto = document.querySelector("#addPhoto");
+  const modalGalery = document.querySelector(".modal_galery");
+  const modalAddPhoto = document.querySelector(".modal_add_photo");
+  const buttonBack = document.querySelector(".button_back");
+  addPhoto.addEventListener('click', () => {
+    modalGalery.classList.add('hidden');
+    modalAddPhoto.classList.remove('hidden');
+  });
+  buttonBack.addEventListener('click', () => {
+    modalAddPhoto.classList.add('hidden');
+    modalGalery.classList.remove('hidden');
+  });
+  }
+
+  showModal();
 
 // Afficher travaux modal
 
@@ -190,7 +195,6 @@ async function displayWorksModal() {
  async function deleteWorks(id, figure) {
   const token = sessionStorage.getItem("token");
 
-  console.log(id);
   await fetch(`http://localhost:5678/api/works/${id}`, {
     method: "DELETE",
     headers: {
@@ -214,24 +218,130 @@ async function displayWorksModal() {
   });
 }
 
-// Fonction afficher l'une ou l'autre modal
+// Fonction pour ajouter les travaux
 
-function showModal() {
-  const addPhoto = document.querySelector("#addPhoto");
-  const modalGalery = document.querySelector(".modal_galery");
-  const modalAddPhoto = document.querySelector(".modal_add_photo");
-  const buttonBack = document.querySelector(".button_back");
-  addPhoto.addEventListener('click', () => {
-    modalGalery.classList.add('hidden');
-    modalAddPhoto.classList.remove('hidden');
+const fileInput = document.getElementById('getFile');
+const descriptionInput = document.getElementById('title-image');
+const categoryInput = document.getElementById('category-image');
+
+
+async function uploadFile() {
+  // Créer une instance de FormData
+  const formData = new FormData();
+
+  // Ajouter le fichier et la description au FormData
+  formData.append('image', fileInput.files[0]);
+  fileInput.value = null;
+  formData.append('title', descriptionInput.value);
+  formData.append('category', categoryInput.value);
+try {
+  // Envoyer la requête avec fetch
+  const response = await fetch('http://localhost:5678/api/works', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    body: formData,
   });
-  buttonBack.addEventListener('click', () => {
-    modalAddPhoto.classList.add('hidden');
+
+  // Vérifier si la réponse est correcte
+  if (response.ok) {
+    const data = await response.json();
+
+    const figuresGallery = document.querySelector('.gallery');
+    const figuresLarges = figuresGallery.querySelectorAll('figure');
+    figuresLarges.forEach(figure => {
+      figure.remove();
+    })
+    const figuresModal = document.querySelectorAll('.thumbnail');
+    figuresModal.forEach(figure => {
+      figure.remove();
+    })
+    displayWorks();
+    displayWorksModal();
+    // Vide le formulaire une fois le projet ajouté
+    resetFormFields();
+    const modalGalery = document.querySelector(".modal_galery");
+    const modalAddPhoto = document.querySelector(".modal_add_photo");
     modalGalery.classList.remove('hidden');
-  });
+    modalAddPhoto.classList.add('hidden');
+  } else {
+    console.error('Erreur lors du téléchargement du fichier:', response.statusText);
   }
+} catch (error) {
+  console.error('Erreur réseau:', error);
+}
+}
 
-  showModal();
+// Vérifie que le formulaire est bien rempli et envoie le fichier
+
+const uploadButton = document.getElementById('uploadButton')
+const errorElement = document.getElementById('error-message');
+
+function validateAndUpload(event) {
+event.preventDefault(); // Empêcher le comportement par défaut du bouton
+
+
+// Vérifier les champs du formulaire
+if (fileInput.files[0] == undefined || descriptionInput.value === '' || categoryInput.value === '0') {
+  // Afficher un message d'erreur si l'un des champs requis n'est pas rempli
+  errorElement.textContent = 'Veuillez remplir tous les champs obligatoires.';
+  errorElement.style.color = 'red';
+} else {
+  errorElement.textContent = '';
+  // Appeler la fonction d'upload si tous les champs sont remplis
+  uploadFile();
+}
+}
+
+// Ajouter un écouteur d'événement sur le clic du bouton de validation
+uploadButton.addEventListener('click', validateAndUpload);
+
+// Afficher miniature image
+
+const getFile = document.getElementById('getFile');
+const preview = document.getElementById('upload-works');
+const uploadWorksElement = document.querySelector('.upload-works img');
+const addPhoto = document.querySelector('.addPhoto');
+const formatPhoto = document.querySelector('.formatPhoto');
+
+
+getFile.addEventListener('change',() => {
+
+preview.src = URL.createObjectURL(getFile.files[0]);
+
+  // Changer la classe de l'élément
+  uploadWorksElement.className = 'thumbnail-img';
+  addPhoto.style.display = 'none';
+  formatPhoto.style.display = 'none';
+})
+
+// Changer la couleur du bouton en fonction de si le formulaire est bien rempli
+  
+// Sélectionnez votre bouton de validation
+const boutonValider = document.getElementById("uploadButton");
+
+// Définissez une fonction pour vérifier l'état des champs et mettre à jour la couleur du bouton
+function verifierChamps() {
+    var champ1 = document.querySelector("#getFile").value;
+    var champ2 = document.querySelector("#title-image").value;
+    var champ3 = document.querySelector("#category-image").value;
+    // Vérifiez toutes les conditions nécessaires pour considérer les champs comme valides
+    
+    if (champ1 !== "" && champ2 !== "" && champ3 !== "0") {
+        boutonValider.classList.add("input_modal");
+        boutonValider.classList.remove("input_modalGrey");
+    } else {
+        boutonValider.classList.remove("input_modal");
+        boutonValider.classList.add("input_modalGrey");
+    }
+}
+
+// Ajoutez des écouteurs d'événements à chaque champ pour appeler la fonction de vérification lorsqu'ils sont modifiés
+document.querySelector("#getFile").addEventListener("input", verifierChamps);
+document.querySelector("#title-image").addEventListener("input", verifierChamps);
+document.querySelector("#category-image").addEventListener("input", verifierChamps);
+
 
 // Fonction pour vider le formulaire une fois le projet validé
 
@@ -267,104 +377,10 @@ function showModal() {
 
 
 
-  // Fonction pour ajouter les travaux
 
-  const fileInput = document.getElementById('getFile');
-  const descriptionInput = document.getElementById('title-image');
-  const categoryInput = document.getElementById('category-image');
+
   
-
-  async function uploadFile() {
-    // Créer une instance de FormData
-    const formData = new FormData();
-  
-    // Ajouter le fichier et la description au FormData
-    formData.append('image', fileInput.files[0]);
-    fileInput.value = null;
-    formData.append('title', descriptionInput.value);
-    formData.append('category', categoryInput.value);
-  try {
-    // Envoyer la requête avec fetch
-    const response = await fetch('http://localhost:5678/api/works', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      },
-      body: formData,
-    });
-
-    // Vérifier si la réponse est correcte
-    if (response.ok) {
-      const data = await response.json();
-      console.log('Fichier téléchargé avec succès:', data);
-      const figuresGallery = document.querySelector('.gallery');
-      const figuresLarges = figuresGallery.querySelectorAll('figure');
-      figuresLarges.forEach(figure => {
-        figure.remove();
-      })
-      const figuresModal = document.querySelectorAll('.thumbnail');
-      figuresModal.forEach(figure => {
-        figure.remove();
-      })
-      displayWorks();
-      displayWorksModal();
-      // Vide le formulaire une fois le projet ajouté
-      resetFormFields();
-      const modalGalery = document.querySelector(".modal_galery");
-      const modalAddPhoto = document.querySelector(".modal_add_photo");
-      modalGalery.classList.remove('hidden');
-      modalAddPhoto.classList.add('hidden');
-    } else {
-      console.error('Erreur lors du téléchargement du fichier:', response.statusText);
-    }
-  } catch (error) {
-    console.error('Erreur réseau:', error);
-  }
-}
-
-// Vérifie que le formulaire est bien rempli et envoie le fichier
-
-const uploadButton = document.getElementById('uploadButton')
-const errorElement = document.getElementById('error-message');
-
-function validateAndUpload(event) {
-  event.preventDefault(); // Empêcher le comportement par défaut du bouton
-  console.log(fileInput.value);
-  console.log(fileInput.files[0]);
-
-  // Vérifier les champs du formulaire
-  if (fileInput.files[0] == undefined || descriptionInput.value === '' || categoryInput.value === '0') {
-    // Afficher un message d'erreur si l'un des champs requis n'est pas rempli
-    errorElement.textContent = 'Veuillez remplir tous les champs obligatoires.';
-    errorElement.style.color = 'red';
-  } else {
-    errorElement.textContent = '';
-    // Appeler la fonction d'upload si tous les champs sont remplis
-    uploadFile();
-  }
-}
-
-// Ajouter un écouteur d'événement sur le clic du bouton de validation
-uploadButton.addEventListener('click', validateAndUpload);
-
-// Afficher miniature image
-
-const getFile = document.getElementById('getFile');
-const preview = document.getElementById('upload-works');
-const uploadWorksElement = document.querySelector('.upload-works img');
-const addPhoto = document.querySelector('.addPhoto');
-const formatPhoto = document.querySelector('.formatPhoto');
-
-
-getFile.addEventListener('change',() => {
-  console.log(getFile.files[0])
-  preview.src = URL.createObjectURL(getFile.files[0]);
-
-    // Changer la classe de l'élément
-    uploadWorksElement.className = 'thumbnail-img';
-    addPhoto.style.display = 'none';
-    formatPhoto.style.display = 'none';
-})
+// CONTENU EN FONCTION DE L'ETAT DE CONNEXION
 
   // Afficher ou masquer le contenu en fonction de l'état de connexion
 
@@ -375,7 +391,7 @@ const privateContent = document.querySelectorAll('.privateContent');
 if (tokenContent) {
   privateContent.forEach (element => {
     element.style.display = 'flex';
-    console.log(element, privateContent);
+
   });
 } else {
   privateContent.forEach(element => {
@@ -405,46 +421,13 @@ const verifyTokenIsPresent = () =>  {
 
   verifyTokenIsPresent();
 
-  // Cache les filtres
-// const filter = document.querySelectorAll('.filter');
-// console.log('Filter elements:', filter);
-// console.log(tokenContent)
-// if (tokenContent) {
-//   filter.forEach (element => {
-//     element.style.display = 'none';
-//     console.log('Hiding filter:', element);
-//   });
-// } else {
-//   filter.forEach(element => {
-//     element.style.display = 'inline-block';
-//     console.log('Display property:', element.style.display);
-//   });
-// }
+// Bande mode édition
 
-
-
-// Changer la couleur du bouton en fonction de si le formulaire est bien rempli
-  
-// Sélectionnez votre bouton de validation
-const boutonValider = document.getElementById("uploadButton");
-
-// Définissez une fonction pour vérifier l'état des champs et mettre à jour la couleur du bouton
-function verifierChamps() {
-    var champ1 = document.querySelector("#getFile").value;
-    var champ2 = document.querySelector("#title-image").value;
-    var champ3 = document.querySelector("#category-image").value;
-    // Vérifiez toutes les conditions nécessaires pour considérer les champs comme valides
-    
-    if (champ1 !== "" && champ2 !== "" && champ3 !== "0") {
-        boutonValider.classList.add("input_modal");
-        boutonValider.classList.remove("input_modalGrey");
-    } else {
-        boutonValider.classList.remove("input_modal");
-        boutonValider.classList.add("input_modalGrey");
-    }
-}
-
-// Ajoutez des écouteurs d'événements à chaque champ pour appeler la fonction de vérification lorsqu'ils sont modifiés
-document.querySelector("#getFile").addEventListener("input", verifierChamps);
-document.querySelector("#title-image").addEventListener("input", verifierChamps);
-document.querySelector("#category-image").addEventListener("input", verifierChamps);
+const header = document.querySelector(".header");
+const adminDiv = document.querySelector(".banner-admin");
+const adminP = document.createElement("p");
+adminDiv.classList.add("banner-admin");
+adminP.classList.add("banner-p");
+adminP.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> Mode édition';
+adminDiv.appendChild(adminP);
+header.appendChild(adminDiv);
